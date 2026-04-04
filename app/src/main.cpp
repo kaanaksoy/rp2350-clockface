b23d663545263fac77c323b7f88cbf9759423258
Kaan Aksoy <kaan.aksoy@kaksoy.com>
2026-04-04 23:49:58 +0300

    Cleaned up main

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -49,16 +49,6 @@ int main() {
   add_repeating_timer_ms(LVGL_TICK_PERIOD_MS, repeating_lvgl_timer_cb, NULL,
                          &lvgl_timer);
 
-  // ClockScreen clock_screen;
-
-  // printf("Pre ClockScreen\n");
-  // clock_time_t current_time = {12, 30, 0};
-  // clock_screen.init(current_time);
-  // for (;;) {
-  //   lv_timer_handler();
-  //   // printf("Loop\n");
-  //   // sleep_ms(1);
-  // }
   App app;
   app.init();
   app.run();
